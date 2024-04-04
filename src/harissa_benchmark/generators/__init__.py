1cb054bfae5efa62364880614b6c5bae50e8cab9
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-04-04 16:45:10 +0200

    update

--- src/harissa_benchmark/generators/__init__.py
+++ src/harissa_benchmark/generators/__init__.py
@@ -1,3 +1,4 @@
 from harissa_benchmark.generators.networks import NetworksGenerator
+from harissa_benchmark.generators.datasets import DatasetsGenerator
 
-__all__ = ['NetworksGenerator']
+__all__ = ['NetworksGenerator', 'DatasetsGenerator']
