27fb9b66dbd6d4eefef891dd2eb2c69d9a4c0971
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-04-08 15:20:24 +0200

    add available inferences

--- src/harissa_benchmark/networks/__init__.py
+++ src/harissa_benchmark/networks/__init__.py
@@ -1,8 +1,8 @@
 from harissa_benchmark.generators import NetworksGenerator
-from harissa_benchmark.utils import collect_modules
+from harissa_benchmark.utils import collect_submodules
 
 __all__ = ['available_networks']
 
-collect_modules(__file__, __all__)
+collect_submodules(__file__, __all__)
 
 available_networks = NetworksGenerator.available_networks
