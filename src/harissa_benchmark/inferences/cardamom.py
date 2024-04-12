855cc27e7ce494aefad2558f94dc8516caadd23d
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-04-12 20:11:47 +0200

    add cardamom

---
+++ src/harissa_benchmark/inferences/cardamom.py
@@ -0,0 +1,4 @@
+from harissa.inference import Cardamom
+from harissa_benchmark.generators import InferencesGenerator
+
+InferencesGenerator.register('Cardamom', Cardamom)
--- src/harissa_benchmark/utils.py
+++ src/harissa_benchmark/utils.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from importlib import import_module
 
-def collect_submodules(init_file, modules):
+def collect_submodules(init_file, submodules):
     module = Path(init_file).parent
     for path in module.iterdir():
         submodule = path.stem
         if not submodule.startswith('_'):
             import_module(f'harissa_benchmark.{module.stem}.{submodule}')
-            modules.append(submodule)
+            submodules.append(submodule)
