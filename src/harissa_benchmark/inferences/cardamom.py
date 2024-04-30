69ad3b52819757a3fcb5dfaea630cde1077078e0
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-04-30 15:30:12 +0200

    add try except in case user not on the good branch

--- src/harissa_benchmark/inferences/cardamom.py
+++ src/harissa_benchmark/inferences/cardamom.py
@@ -1,14 +1,17 @@
-import numpy as np
+try:
+    import numpy as np
+    from harissa.inference import Cardamom
+    from harissa_benchmark.generators import InferencesGenerator, InferenceInfo
 
-from harissa.inference import Cardamom
-from harissa_benchmark.generators import InferencesGenerator, InferenceInfo
-
-InferencesGenerator.register(
-    'Cardamom', 
-    InferenceInfo(
-        Cardamom, 
-        True, 
-        np.array([InferencesGenerator.color_map(8), 
-                  InferencesGenerator.color_map(9)])
+    InferencesGenerator.register(
+        'Cardamom', 
+        InferenceInfo(
+            Cardamom, 
+            True, 
+            np.array([InferencesGenerator.color_map(8), 
+                    InferencesGenerator.color_map(9)])
+        )
     )
-)
+except ImportError as e:
+    from sys import stderr
+    print(e.msg, file=stderr)
