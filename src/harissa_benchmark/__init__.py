a1ee879fa9cf7788ecba9d9e7d7342c14f00a706
Nathaniel Seyler <nathaniel.seyler@loria.fr>
2024-05-07 14:30:00 +0200

    update

--- src/harissa_benchmark/__init__.py
+++ src/harissa_benchmark/__init__.py
@@ -8,7 +8,9 @@ from alive_progress import config_handler as _cfh
 __all__ = ['Benchmark', 'available_networks', 'available_inferences']
 
 _cfh.set_global(
+    bar='smooth',
     spinner=_bsf('🌶', 6, hide=False),
+    unknown='horizontal',
     # dual_line=True,
     receipt=False,
     force_tty=True,
